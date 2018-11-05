5260d337b7515adb4a4ee239e843f6c8d620ff19
Julien Prigent <julienprigent@wanadoo.fr>
2018-11-05 22:05:31 +0000

    add insert-a-node-at-a-specific-position-in-a-linked-list.py

---
+++ Algorithm/linked-list/insert-a-node-at-a-specific-position-in-a-linked-list.py
@@ -0,0 +1,50 @@
+
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+class SinglyLinkedListNode:
+    def __init__(self, node_data):
+        self.data = node_data
+        self.next = None
+
+class SinglyLinkedList:
+    def __init__(self):
+        self.head = None
+        self.tail = None
+
+    def insert_node(self, node_data):
+        node = SinglyLinkedListNode(node_data)
+
+        if not self.head:
+            self.head = node
+        else:
+            self.tail.next = node
+
+
+        self.tail = node
+
+def print_singly_linked_list(node, sep, fptr):
+    while node:
+        fptr.write(str(node.data))
+
+        node = node.next
+
+        if node:
+            fptr.write(sep)
+
+
+def insertNodeAtPosition(head, data, position):
+    i = 1
+    root = head
+    while i != position and head is not None:
+      head = head.next
+      i += 1
+    node = SinglyLinkedListNode(data)
+    node.next = head.next
+    head.next = node
+    return root
